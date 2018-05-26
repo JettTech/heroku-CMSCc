ee551e2d6713cce1ee6d180ca0c065a6e92945a5
Lisa Jetton <ldjetton21@gmail.com>
2018-05-26 01:19:37 -0500

    updates to css

---
+++ models/seedsForMongooseFields.js
@@ -0,0 +1,130 @@
+/* 1 */
+{
+    "_id" : ObjectId("5afa4495fc90363f20830412"),
+    "title" : "full-demolition",
+    "slug" : "full-demolition",
+    "description" : "<p>Demolition of Appartments</p>",
+    "price" : 1499.95,
+    "category" : "demolition",
+    "image" : "craneAndConcrete.jpg",
+    "__v" : 0.0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5b071e532485d4251a546eb6"),
+    "title" : "retile-first-floor",
+    "slug" : "retile-first-floor",
+    "description" : "<p>Retile entire first floor of 5600 sq ft home.</p>",
+    "price" : 1799.89,
+    "category" : "flooring",
+    "image" : "roomUnderConstruction.jpg",
+    "__v" : 0.0
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("5b084c9e7a05850004f10e71"),
+    "title" : "study-window-installation",
+    "slug" : "study-window-installation",
+    "description" : "<p>Full furnishing of study windows in a studio apartment.</p>",
+    "price" : 689,
+    "category" : "window-placement",
+    "image" : "pureArchitecture.jpg",
+    "__v" : 0
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("5b084d277a05850004f10e72"),
+    "title" : "replacement-plumbing",
+    "slug" : "replacement-plumbing",
+    "description" : "<p>Plumbing repair in the upstairs family kitchen.</p>",
+    "price" : 7896.1,
+    "category" : "plumbing",
+    "image" : "jason-briscoe-332507-unsplash.jpg",
+    "__v" : 0
+}
+
+/* 5 */
+{
+    "_id" : ObjectId("5b084d747a05850004f10e73"),
+    "title" : "international-school-building",
+    "slug" : "international-school-building",
+    "description" : "<p>Develop and construct additional 2 rooms for school.</p>",
+    "price" : 566.76,
+    "category" : "commercial-development",
+    "image" : "dayWorkers.jpg",
+    "__v" : 0
+}
+
+/* 6 */
+{
+    "_id" : ObjectId("5b084da77a05850004f10e74"),
+    "title" : "caterpillar-rental",
+    "slug" : "caterpillar-rental",
+    "description" : "<p>Rent the Caterpillar for industrial excavation sites.</p>",
+    "price" : 106.99,
+    "category" : "hardware-rental",
+    "image" : "catepillarCraneInDirt.jpg",
+    "__v" : 0
+}
+
+/* 7 */
+{
+    "_id" : ObjectId("5b084e497a05850004f10e75"),
+    "title" : "commercial-corridor-redevelopment",
+    "slug" : "commercial-corridor-redevelopment",
+    "description" : "<p>Develop construction blueprint and reconstruct commercial corridor off Hwy I-80.</p>",
+    "price" : 5400,
+    "category" : "renovation",
+    "image" : "heavyMetalShop.jpg",
+    "__v" : 0
+}
+
+/* 8 */
+{
+    "_id" : ObjectId("5b084eea7a05850004f10e76"),
+    "title" : "media-room-electrics",
+    "slug" : "media-room-electrics",
+    "description" : "<p>Install full electric outlets and wiring for all&nbsp;electronics&nbsp;in the media room.</p>",
+    "price" : 689,
+    "category" : "electric-work",
+    "image" : "rob-lambert-64186-unsplash.jpg",
+    "__v" : 0
+}
+
+/* 9 */
+{
+    "_id" : ObjectId("5b084f5e7a05850004f10e77"),
+    "title" : "new-start-painting-job",
+    "slug" : "new-start-painting-job",
+    "description" : "<p>Whitewash and repaint all living areas of 2-story 3200 sq ft home.</p>",
+    "price" : 359,
+    "category" : "painting",
+    "image" : "aaron-huber-401151-unsplash.jpg",
+    "__v" : 0
+}
+
+/* 10 */
+{
+    "_id" : ObjectId("5b084fee7a05850004f10e78"),
+    "title" : "new-home-design",
+    "slug" : "new-home-design",
+    "description" : "<p>Strategically prepare and certify building plans for new home construction.</p>",
+    "price" : 989,
+    "category" : "blueprint-design",
+    "image" : "alexander-andrews-457319-unsplash.jpg",
+    "__v" : 0
+}
+
+/* 11 */
+{
+    "_id" : ObjectId("5b0850777a05850004f10e79"),
+    "title" : "hail-damage-appraisal",
+    "slug" : "hail-damage-appraisal",
+    "description" : "<p>Assess full damage from a recent hail storm, and deliver detailed repair report.</p>",
+    "price" : 751,
+    "category" : "appraisal",
+    "image" : "vidar-nordli-mathisen-606813-unsplash.jpg",
+    "__v" : 0
